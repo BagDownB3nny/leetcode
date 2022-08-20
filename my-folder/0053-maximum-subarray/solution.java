efaf1c1d79c7c82524acc68fe0eafa4048e98526
Darren Sim <e0725166@u.nus.edu>
2022-08-20 10:15:57 +0000

    [LeetCode Sync] Runtime - 3 ms (5.10%), Memory - 73.8 MB (5.44%)

--- my-folder/0053-maximum-subarray/solution.java
+++ my-folder/0053-maximum-subarray/solution.java
@@ -1,23 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = 0;
-        int section = 0;
-        int greatestNegative = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
+        int currentMax = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > greatestNegative && nums[i] <= 0) {
-                greatestNegative = nums[i];
+            currentMax += nums[i];
+            if (currentMax > max) {
+                max = currentMax;
             }
-            section += nums[i];
-            if (section < 0) {
-                section = 0;
-                continue;
+            if (currentMax < 0) {
+                currentMax = 0;
             }
-            if (section > max) {
-                max = section;
-            }
-        }
-        if (max == 0) {
-            return greatestNegative;
         }
         return max;
     }
