c0a0959abc571603bf08697e4a3504706b2c026b
Darren Sim <e0725166@u.nus.edu>
2023-08-25 10:49:58 +0000

    [LeetCode Sync] Runtime - 11 ms (83.12%), Memory - 13.5 MB (8.31%)

--- my-folder/0020-valid-parentheses/solution.py
+++ my-folder/0020-valid-parentheses/solution.py
@@ -1,23 +1,19 @@
 class Solution(object):
     def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        open_brackets = ['(', '{', '[']
+        close_brackets = [')', '}', ']']
         stack = []
-        for bracket in s:
-            print(bracket)
-            if bracket == ')':
-                if len(stack) == 0:
-                    return False
-                if stack.pop() != '(':
-                    return False
-            elif bracket == ']':
-                if len(stack) == 0:
-                    return False
-                if stack.pop() != '[':
-                    return False
-            elif bracket == '}':
-                if len(stack) == 0:
-                    return False
-                if stack.pop() != '{':
-                    return False
+        for char in s:
+            if char in open_brackets:
+                stack.append(char)
             else:
-                stack.append(bracket)
+                if (len(stack) == 0):
+                    return False
+                prev_char = stack.pop()
+                if open_brackets.index(prev_char) != close_brackets.index(char):
+                    return False
         return len(stack) == 0
