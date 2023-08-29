b0fe066b6291c6237991ed968ff9a00592c120bb
Darren Sim <e0725166@u.nus.edu>
2023-08-29 22:12:01 +0000

    [LeetCode Sync] Runtime - 65 ms (9.31%), Memory - 13.7 MB (10.02%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,19 +1,23 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        letters_dict = {}
-        for letter in s:
-            if letter in letters_dict:
-                letters_dict[letter] += 1
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        hashmap = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+            if char in hashmap.keys():
+                hashmap[char] = hashmap[char] + 1
             else:
-                letters_dict[letter] = 1
-        for letter in t:
-            if letter in letters_dict:
-                if letters_dict[letter] == 1:
-                    letters_dict.pop(letter)
-                else:
-                    letters_dict[letter] -= 1
+                hashmap[char] = 1
+        for char in t:
+            if char in hashmap.keys():
+                hashmap[char] = hashmap[char] - 1
+                if hashmap[char] < 0:
+                    return False
             else:
                 return False
-        return len(letters_dict) == 0
-        
-        
+        return True
