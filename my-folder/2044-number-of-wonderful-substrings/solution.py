687e9fac8f79b8a04cb95a7de0408f1f3597d840
Darren Sim <e0725166@u.nus.edu>
2024-04-30 19:59:48 +0000

    [LeetCode Sync] Runtime - 1136 ms (66.80%), Memory - 17.4 MB (96.84%)

---
+++ my-folder/2044-number-of-wonderful-substrings/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+
+        seenBitmaps = [1] + [0] * 1023
+        ans = 0
+        bitmap = 0
+        for c in word:
+            bitmap ^= 1 << (ord(c) - ord("a"))
+            ans += seenBitmaps[bitmap]
+            seenBitmaps[bitmap] += 1
+            for n in range(10):
+                m = bitmap ^ 1 << n
+                ans += seenBitmaps[m]
+        return ans
+            
